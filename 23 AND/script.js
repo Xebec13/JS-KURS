3cd6d88539ae8f1bd162668e5c4eb9d959a13354
David <dhoesen@gmail.com>
2024-03-05 12:33:37 +0100

    and or

---
+++ 23 AND/script.js
@@ -0,0 +1,19 @@
+
+console.log(true && true);
+console.log(true && false);
+
+
+
+console.log(5 >= 5 && 1 < 4);  // true
+console.log(7 == 8 && 1 < 4);  // false
+
+/*
+true #1
+false #2
+false #3
+false #4
+true #5
+false #6
+true #7
+true #8
+*/
